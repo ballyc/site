f0b8b539e472b9a9da4e04d14edac0414b26cb29
ballyc <ballyc@umich.edu>
2026-06-04 11:38:34 -0400

    website update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
       <h2>## Projects</h2>
 
       <ul>
-        <li><span className="proj">*/company-brief*</span>: Custom skill; analyzes companies. Reports saved in Obsidian. Indexed + searchable. ROI 15x.<br></br>Run on Claude Code (2026) [ <a href="https://github.com/ballyc/company-brief" target="_blank">repo</a>, <a href="https://github.com/ballyc/company-brief/blob/main/SKILL.md" target="_blank">skill.md</a>, <a href="https://github.com/ballyc/company-brief/blob/main/Example/Hebbia.md" target="_blank">output example with Hebbia</a> ]</li>
-        <li><span className="proj">*how-do-you-think*</span> Mini app; analyzes language pattern from notes. Returns structural mirror of how you think, reason, discover, and learn. <br></br>Using vercel (2026) [ <a href="https://how-do-you-think.vercel.app" target="_blank">site</a>, <a href="https://github.com/ballyc/how-do-you-think" target="_blank">repo</a> ]</li>
+        <li><span className="proj">*/company-brief*</span>: Custom skill; analyzes companies. Reports saved in Obsidian. Indexed + searchable. ROI 15x.<br></br>Runs on Claude Code (2026) [ <a href="https://github.com/ballyc/company-brief" target="_blank">repo</a>, <a href="https://github.com/ballyc/company-brief/blob/main/SKILL.md" target="_blank">skill.md</a>, <a href="https://github.com/ballyc/company-brief/blob/main/Example/Hebbia.md" target="_blank">output example with Hebbia</a> ]</li>
+        <li><span className="proj">*how-do-you-think*</span> Mini app, just trying out Vercel for fun; input an introspective note, returns insight on your thinking patterns. from simple prompt (yet engineered by 170 psycho-analytics string data as input). <br></br>Runs on vercel + uses claude-opus-4-5 (2026) [ <a href="https://how-do-you-think.vercel.app" target="_blank">site</a>, <a href="https://github.com/ballyc/how-do-you-think" target="_blank">repo</a> ]</li>
       </ul> 
 
 
